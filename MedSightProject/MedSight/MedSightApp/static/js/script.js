df92b69ef10b07b8a2831c343e494d5945b1c7f7
mikerossti <93906668+mikerossti@users.noreply.github.com>
2021-12-02 15:41:37 +0800

    appointment datatables fix
    search and pagination not working yet

--- MedSightProject/MedSight/MedSightApp/static/js/script.js
+++ MedSightProject/MedSight/MedSightApp/static/js/script.js
@@ -8,3 +8,11 @@ $(document).ready(function() {
         "lengthMenu": [[5, 10, 15, -1], [5, 10, 15, "All"]]
     });
 } );
+
+$(document).ready( function () {
+    $('#appointmentsdatatable').DataTable({
+        "lengthMenu": [[5, 10, 15, -1], [5, 10, 15, "All"]],
+        "searching": true,
+        "filter": true,
+    });
+});
